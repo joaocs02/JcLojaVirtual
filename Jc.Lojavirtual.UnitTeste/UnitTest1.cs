cccddc7c25c51852cbd614ce1e46e70ff8bf6120
joaoc <joaocs02@gmail.com>
2019-07-22 10:19:13 -0300

    Add project files.

---
+++ Jc.Lojavirtual.UnitTeste/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jc.Lojavirtual.UnitTeste
+{
+    [TestClass]
+    public class UnitTestJc
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
