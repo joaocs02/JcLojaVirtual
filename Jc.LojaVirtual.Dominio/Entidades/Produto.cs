011b86ca8a649237b392e51a617dcdead12ac95b
joaoc <joaocs02@gmail.com>
2019-07-24 15:35:21 -0300

    ClsProduto

---
+++ Jc.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jc.LojaVirtual.Dominio.Entidades
+{
+
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+        public string nome { get; set; }
+
+        public string descricap { get; set; }
+
+        public decimal preco { get; set; }
+
+        public string categoria { get; set; }
+
+    }
+    
+}
